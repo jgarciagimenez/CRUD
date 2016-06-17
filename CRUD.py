fbc4e77e50aeb06787086ac0589ff0d2e1c257e2
jgarciagimenez <josemanuelgarciagimenez@gmail.com>
2016-06-17 13:43:26 +0200

    Menus and handler defined

--- CRUD.py
+++ CRUD.py
@@ -2,34 +2,122 @@
 
 from gi.repository import Gtk
 
-# class Handler:
-#     def __init__(self):
-#         self.__valor = True
 
-#     def onDeleteWindow(self, *args):
-#         Gtk.main_quit(*args)
 
-#     def onButtonPressed(self, button):
-#         print("Hello World!")
+
+class Handler:
+
+    Builder = None
+
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("menu_CRUD.glade")
+        self.handlers = { "on_window_destroy": self.on_window_destroy,
+                          "on_btn_accept_clicked_RD": self.on_btn_accept_clicked_RD,
+                          "on_btn_cancel_clicked_RD": self.on_btn_cancel_clicked_RD,
+                          "on_btn_accept_clicked_CU": self.on_btn_accept_clicked_CU,
+                          "on_btn_cancel_clicked_CU": self.on_btn_cancel_clicked_CU,
+                          "on_Create_Activate": self.on_Create_Activate,
+                          "on_Update_Activate": self.on_Update_Activate,
+                          "on_Read_Activate": self.on_Read_Activate,
+                          "on_Delete_Activate": self.on_Delete_Activate,
+                          "on_Close_Dia_CU": self.on_Close_Dia_CU,
+                          "on_Close_Dia_RD": self.on_Close_Dia_RD }
+   
+
+        # Conectamos las señales e iniciamos la aplicación
+        self.builder.connect_signals(self.handlers)
+        self.window = self.builder.get_object("window")
+        self.dia_CU = self.builder.get_object("dia_CU")
+        self.dia_RD = self.builder.get_object("dia_RD")       
+        self.entry_RD = self.builder.get_object("entry_RD")
+        self.entry_DB_1 = self.builder.get_object("entry_DB_1")
+        self.entry_DB_2 = self.builder.get_object("entry_DB_2")           
+        self.entry_DB_3 = self.builder.get_object("entry_DB_3")
+        self.entry_DB_4 = self.builder.get_object("entry_DB_4")
+        self.entry_DB_5 = self.builder.get_object("entry_DB_5")
+        self.entry_DB_6 = self.builder.get_object("entry_DB_6")
+
+        self.window.show_all()
+
+   
+    def on_window_destroy(self, *args):
+        print 'Se ha cerrado la ventana'
+        Gtk.main_quit(*args)
+
+
+    def on_Create_Activate(self, *args):
+        self.dia_CU.show()
+
+    def on_Update_Activate(self, *args):
+        self.dia_CU.show()
+
+    def on_Read_Activate(self, *args):
+        self.dia_RD.show()
+
+    def on_Delete_Activate(self, *args):
+        self.dia_RD.show()
+
+
+
+    def on_btn_accept_clicked_RD(self,*args):
+
+        self.entry_RD.set_text('')
+        self.dia_RD.hide()
         
-#     def onButtonClick(self, button):
-#         if self.__valor:
-#             self.__btnLabel = button.get_label()
-#             button.set_label("Contenido nuevo")
-#             self.__valor = False
-#         else:
-#             self.__btnLabel = button.get_label()
-#             button.set_label("button2")
-#             self.__valor = True
-#         print self.__btnLabel
-#     valor = True
-#     btnLabel = ""
 
-builder = Gtk.Builder()
-builder.add_from_file("menu_CRUD.glade")
-#builder.connect_signals(Handler())
+    def on_btn_cancel_clicked_RD(self,*args):
 
-window = builder.get_object("window1")
-window.show_all()
+        self.entry_RD.set_text('')       
+        self.dia_RD.hide()
+        
 
-Gtk.main()
+    def on_btn_accept_clicked_CU(self,*args):
+
+        self.entry_DB_1.set_text('') 
+        self.entry_DB_2.set_text('') 
+        self.entry_DB_3.set_text('') 
+        self.entry_DB_4.set_text('') 
+        self.entry_DB_5.set_text('') 
+        self.entry_DB_6.set_text('') 
+
+        self.dia_CU.hide()
+        
+
+    def on_btn_cancel_clicked_CU(self,*args):
+
+        self.entry_DB_1.set_text('') 
+        self.entry_DB_2.set_text('') 
+        self.entry_DB_3.set_text('') 
+        self.entry_DB_4.set_text('') 
+        self.entry_DB_5.set_text('') 
+        self.entry_DB_6.set_text('')   
+
+        self.dia_CU.hide()    
+ 
+
+    def on_Close_Dia_CU(self):
+
+        self.entry_DB_1.set_text('') 
+        self.entry_DB_2.set_text('') 
+        self.entry_DB_3.set_text('') 
+        self.entry_DB_4.set_text('') 
+        self.entry_DB_5.set_text('') 
+        self.entry_DB_6.set_text('') 
+
+        self.dia_CU.hide()
+
+
+    def on_Close_Dia_RD(self):
+
+        self.entry_RD.set_text('')
+        self.dia_RD.hide()
+
+
+def main():
+    window = Handler()
+    Gtk.main()
+    return 0
+
+if __name__ == '__main__':
+    main()
